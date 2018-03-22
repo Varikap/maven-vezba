a732a4022ccde78913aaa046af2115fedfb580ea
Varikap <popadicn@gmail.com>
2018-03-22 11:35:08 +0100

    Changed compiler to 1.9

--- src/main/java/kael/example/vezba/Application.java
+++ src/main/java/kael/example/vezba/Application.java
@@ -1,14 +1,22 @@
 package kael.example.vezba;
 
+import java.util.*;
+
 public class Application {
-    
+    public void greet(){
+    List<String> greetings = new ArrayList<>();
+    greetings.add("Hello");
+    for (String g : greetings) {
+	     System.out.println("greetings" + g);
+	}
+    }
     public Application() {
         System.out.println ("Inside Application");
     }
-
-    // method main(): ALWAYS the APPLICATION entry point
+    
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	app.greet();
     }
 }
