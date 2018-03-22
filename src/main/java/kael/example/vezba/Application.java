f2d8f96875b6a0023c5e13a756f609b500787775
Varikap <popadicn@gmail.com>
2018-03-22 11:51:33 +0100

    Use StringUtils and dependencies

--- src/main/java/kael/example/vezba/Application.java
+++ src/main/java/kael/example/vezba/Application.java
@@ -1,8 +1,17 @@
 package kael.example.vezba;
 
 import java.util.*;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+
+    public int countWords(String words) {
+	String[] sW = StringUtils.split(words,' ');
+	return (sW == null) ? 0 : sW.length;
+
+
+    }
+
     public void greet(){
     List<String> greetings = new ArrayList<>();
     greetings.add("Hello");
@@ -18,5 +27,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
 	app.greet();
+	int count = app.countWords("I have four wordsxD");
+	System.out.println("Word Count: " + count);
     }
 }
